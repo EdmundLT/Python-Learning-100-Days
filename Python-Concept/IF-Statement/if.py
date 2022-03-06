0ffb6eb15ab82b368203b49b99749364992e1bd2
EdmundTang <98913678+EdmundLT@users.noreply.github.com>
2022-03-05 22:30:48 -0500

    Create if.py

---
+++ Python-Concept/IF-Statement/if.py
@@ -0,0 +1,11 @@
+condition = True
+answer = input(”type ‘y’ or ‘n’ to change conditions.”)
+if answer == “y”:
+	condition = True
+if answer == “n”:
+	condition = False
+
+#these 4 line are useless
+#can convert to
+if input(”type ‘y’ or ‘n’ to change condition.”) = n:
+	condition = False
