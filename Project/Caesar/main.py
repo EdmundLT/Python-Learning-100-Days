b78a1de8839cf80afad25789cd3188befecd963c
EdmundTang <98913678+EdmundLT@users.noreply.github.com>
2022-03-05 23:26:30 -0500

    Create main.py

---
+++ Project/Caesar/main.py
@@ -0,0 +1,46 @@
+# Day 8 Exercise - Caesar
+
+- **Code**
+    
+    ```python
+    from art import logo
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+    print(logo)
+    
+    def caesar(start_text, shift_amount, cipher_direction):
+        end_text = ""
+        if cipher_direction == "decode":
+            shift_amount *= -1
+        for char in start_text:
+            if char in alphabet:
+                position = alphabet.index(char)
+                new_position = position + shift_amount
+                end_text += alphabet[new_position]
+            else:
+                end_text += char
+    
+        print(f"Here's the {cipher_direction}d result: {end_text}")
+    
+    # TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+    # e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+    # If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+    # Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+    
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    shift = shift % 26
+    switch = True
+    
+    while switch == True:
+        caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+        ans = input(
+            "Type 'yes' if you want to go again. Otherwise type 'no'").lower()
+        if ans == "yes":
+            switch == True
+        else:
+            switch == False
+    ```
