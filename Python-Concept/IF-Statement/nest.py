18673a4a5fb9864472c146a5a9c33800d9a6f9ed
EdmundTang <98913678+EdmundLT@users.noreply.github.com>
2022-03-05 22:33:01 -0500

    Create nest.py

---
+++ Python-Concept/IF-Statement/nest.py
@@ -0,0 +1,10 @@
+condition = True
+index = 1
+
+if condition:
+  if index == 1:
+    print("condition is true and index is 1")
+   else:
+    print("condition is true but index is not 1")
+else:
+  print("condition is flase")
