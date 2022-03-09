aa8ac0c9ee7f829b6b3047822266c8371001b64a
EdmundTang <98913678+EdmundLT@users.noreply.github.com>
2022-03-09 14:23:34 -0500

    Update main.py

--- Project/Day1-15/Day2-Tip-Calculator/main.py
+++ Project/Day1-15/Day2-Tip-Calculator/main.py
@@ -1,14 +1,3 @@
-"""
-If the bill was $150.00, split between 5 people, with 12% tip.
-
-Each person should pay (150.00 / 5) * 1.12 = 33.6
-
-Format the result to 2 decimal places = 33.60
-
-Thus everyone's share of the total bill is $30.00 plus a $3.60 tip.
-
-Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-"""
 
 print("Welcome to the tip calculator!")
 total_bill = float(input("What was the total bill? "))
