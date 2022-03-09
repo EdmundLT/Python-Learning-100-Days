1989f411750a513f705ff8c24225cb28e46c142f
EdmundTang <98913678+EdmundLT@users.noreply.github.com>
2022-03-09 18:32:52 -0500

    Create main.py

---
+++ Project/Day31-45/Pixela (API)/main.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "ptesting977"
+TOKEN = "asjdas123asdlfkj"
+user_params = {
+    "token": "asjdas123asdlfkj",
+    "username": "ptesting977",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+# Create new Pixela Account
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": "graph2",
+    "name": "Hours of Learning Coding",
+    "unit": "hours",
+    "type": "float",
+    "color": "sora",
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# Create new Graph
+# response = requests.post(
+#     url=graph_endpoint, json=graph_config, headers=headers)
+
+today = datetime.now()
+formatted_date = today.strftime("%Y%m%d")
+pixel_endpoint = f"{graph_endpoint}/graph2"
+quantity = input("Hours learned today?: ")
+pixel_config = {
+    "date": formatted_date,
+    "quantity": quantity,
+}
+
+# recording new data
+response = requests.post(
+    url=pixel_endpoint, json=pixel_config, headers=headers)
+print(response.text)
+
+modify_pixel_endpoint = f"{pixel_endpoint}/{formatted_date}"
+
+modify_config = {
+    "quantity": "4.5",
+}
