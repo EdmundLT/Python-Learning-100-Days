4f01ebf50e32d03aa45765b42b45494abd4f319f
EdmundTang <98913678+EdmundLT@users.noreply.github.com>
2022-03-17 20:04:32 -0400

    Update main.py

--- Project/Day46-60/Day 50 Tinder Auto Swipper(Scrapping)/main.py
+++ Project/Day46-60/Day 50 Tinder Auto Swipper(Scrapping)/main.py
@@ -1,60 +1,75 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
-from time import sleep
 
-FB_EMAIL = YOUR FACEBOOK LOGIN EMAIL
-FB_PASSWORD = YOUR FACEBOOK PASSWORD
-
-chrome_driver_path = YOUR CHROME DRIVER PATH
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
-
-driver.get("http://www.tinder.com")
-
-sleep(2)
-login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+EMAIL = "abc@gmail.com"
+PW = "123"
+driver_path = "chromedriver"
+driver = webdriver.Chrome(executable_path=driver_path)
+driver.get("https://tinder.com")
+login_button = driver.find_element_by_xpath(
+    "/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/span")
 login_button.click()
-
-sleep(2)
-fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+time.sleep(3)
+fb_login = driver.find_element_by_xpath(
+    "/html/body/div[2]/div/div/div[1]/div/div[3]/span/div[2]/button/span[2]")
 fb_login.click()
-
-sleep(2)
+time.sleep(3)
 base_window = driver.window_handles[0]
 fb_login_window = driver.window_handles[1]
 driver.switch_to.window(fb_login_window)
 print(driver.title)
-
-email = driver.find_element_by_xpath('//*[@id="email"]')
-password = driver.find_element_by_xpath('//*[@id="pass"]')
-
-email.send_keys(FB_EMAIL)
-password.send_keys(FB_PASSWORD)
-password.send_keys(Keys.ENTER)
+email_input = driver.find_element_by_xpath(
+    '//*[@id="email"]')
+pw_input = driver.find_element_by_xpath(
+    '//*[@id="pass"]')
+# driver.quit()
+time.sleep(3)
+email_input.send_keys(EMAIL)
+pw_input.send_keys(PW)
+pw_input.send_keys(Keys.ENTER)
+time.sleep(2)
+continue_button = driver.find_element_by_xpath(
+    '//*[@id="mount_0_0_0F"]/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div/div/div/div[1]/div/span/span')
+continue_button.send_keys(Keys.ENTER)
 
 driver.switch_to.window(base_window)
 print(driver.title)
-
-sleep(5)
-allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+# Allow location
+allow_location_button = driver.find_element_by_xpath(
+    '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
 allow_location_button.click()
-notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
-notifications_button.click()
-cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
-cookies.click()
 
+# Disallow notifications
+notifications_button = driver.find_element_by_xpath(
+    '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+
+# Allow cookies
+cookies = driver.find_element_by_xpath(
+    '//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
+# Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
 for n in range(100):
-    sleep(1)
+
+    # Add a 1 second delay between likes.
+    time.sleep(1)
+
     try:
         print("called")
         like_button = driver.find_element_by_xpath(
             '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
         like_button.click()
+
+    # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
     except ElementClickInterceptedException:
         try:
             match_popup = driver.find_element_by_css_selector(".itsAMatch a")
             match_popup.click()
+
+        # Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
         except NoSuchElementException:
-            sleep(2)
+            time.sleep(2)
 
 driver.quit()
